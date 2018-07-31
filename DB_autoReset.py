43d3d4f7d0268c1a04d0be7b91d3bad4cca921ba
Tony Feng <tfeng@fortinet-us.com>
2018-07-31 14:37:15 -0700

    Created function to auto delete migration file (IN PROGRESS)

--- DB_autoReset.py
+++ DB_autoReset.py
@@ -1,6 +1,19 @@
 import os
+import glob
+TeamSQL_DIR = "."
+Api_DIR = "."
+Migration_file_name = "."
 
-TeamSQL_DIR = ""
+def del_migration_files(path, file_name):
+    for name in glob.glob(path):
+        if name = filename:
+            return complete_path = path + filename
+        else:
+            print ('cannot find the file specified')
+            os._exit()
+
+
+
 
 print('====================================================')
 print('This script is used to simplify the DB-reset process')
@@ -8,4 +21,7 @@ print('====================================================')
 
 original_dir = os.getcwd()
 print("The original directory is %s", original_dir)
+
+os.chdir(Api_DIR)
+os.system()
 os.chdir(TeamSQL_DIR)
